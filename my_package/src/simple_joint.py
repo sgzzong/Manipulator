f908284a3152384246eac7ff95b86f57b62906f0
sgzzong <home05296@naver.com>
2022-07-12 10:54:40 +0900

    Add files via upload

---
+++ my_package/src/simple_joint.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from tf.transformations import quaternion_from_euler
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+class MoveGroupPythonIntefaceTutorial(object):
+    def __init__(self):
+        super(MoveGroupPythonIntefaceTutorial, self).__init__()
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
+        robot = moveit_commander.RobotCommander()
+        scene = moveit_commander.PlanningSceneInterface()
+        group_name = "manipulator"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
+        display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                    moveit_msgs.msg.DisplayTrajectory,
+                                                    queue_size=20)        
+        group_names = robot.get_group_names()
+        self.move_group = move_group
+
+    def go_to_joint_state(self):
+        move_group = self.move_group
+        joint_goal = move_group.get_current_joint_values()
+        joint_goal[0] = pi/4
+        joint_goal[1] = -pi/4
+        joint_goal[2] = 0
+        joint_goal[3] = 0
+        joint_goal[4] = 0
+        joint_goal[5] = 0
+        move_group.go(joint_goal, wait=True)
+        move_group.stop()
+def main():
+    tutorial = MoveGroupPythonIntefaceTutorial()
+    tutorial.go_to_joint_state()
+if __name__ == '__main__':
+  main()
